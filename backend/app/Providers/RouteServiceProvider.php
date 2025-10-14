dc89d5b361c77ce61d6e8cecd404648449191700
ndphat123 <ndphat101222@gmail.com>
2025-10-14 10:05:20 +0700

    thêm provides

--- backend/app/Providers/RouteServiceProvider.php
+++ backend/app/Providers/RouteServiceProvider.php
@@ -2,23 +2,30 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
 
 class RouteServiceProvider extends ServiceProvider
 {
     /**
-     * Define your route model bindings, pattern filters, and other route configuration.
+     * Register services.
      */
-    public function boot(): void
+    public function register(): void
     {
-        $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-        });
+        //
     }
+
+    /**
+     * Bootstrap services.
+     */
+public function boot(): void
+{
+    Route::prefix('api')
+        ->middleware('api')
+        ->group(base_path('routes/api.php'));
+
+    Route::middleware('web')
+        ->group(base_path('routes/web.php'));
+}
 }
