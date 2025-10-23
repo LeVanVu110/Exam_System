a759e6bd449f67248a13a54c9ba6b8ed4d3547af
ndphat123 <ndphat101222@gmail.com>
2025-10-23 07:58:59 +0700

    test chạy

--- student-app/src/main/java/com/example/studentapp/Main.java
+++ student-app/src/main/java/com/example/studentapp/Main.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 300);
         stage.setTitle("Student App");
         stage.setScene(scene);
