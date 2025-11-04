e5d5b4c3b101419e330aca17e3b31066e29c11ca
vukha12 <kha15964@gmail.com>
2025-11-04 16:00:09 +0700

    sữa lại dữ liệu bên JavaFX là call API real, thêm các API bên backend trong ExamsSeesionContrnller

--- student-app/src/main/java/com/example/studentapp/model/RoomResponse.java
+++ student-app/src/main/java/com/example/studentapp/model/RoomResponse.java
@@ -3,24 +3,29 @@ package com.example.studentapp.model;
 import java.util.List;
 
 public class ApiResponse {
+    private boolean success;
     private String message;
+    private String date;
     private int count;
 
     private List<RoomModel> data;
 
+    public Boolean getSuccess() {return success;}
+    public void setSuccess(boolean success) {this.success = success;}
 
     public String getMessage() {
         return message;
     }
-
     public void setMessage(String message) {
         this.message = message;
     }
 
+    public String getDate() {return date;}
+    public void setDate(String date) {this.date = date;}
+
     public int getCount() {
         return count;
     }
-
     public void setCount(int count) {
         this.count = count;
     }
@@ -28,7 +33,6 @@ public class ApiResponse {
     public List<RoomModel> getData() {
         return data;
     }
-
     public void setData(List<RoomModel> data) {
         this.data = data;
     }
